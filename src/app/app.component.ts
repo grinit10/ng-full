92ea3ed968f4b931bf1a0910311725a8e6d314c0
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-12-19 09:55:08 +0530

    Completing Template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,10 +7,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultValue = 'pet';
+  questionAnswer = '';
+  genders = ['Male', 'Female'];
+
+  @ViewChild('f') signupForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.value.userData.username = suggestedName;
+    this.signupForm.setValue(this.signupForm.value);
   }
 
-  onsubmit = (f: NgForm) => {console.log(f);
+  // onsubmit = (f: NgForm) => {console.log(f);
+  onsubmit = () => {console.log(this.signupForm);
   }
 }
