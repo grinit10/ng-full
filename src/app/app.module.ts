1e8633867c4f334420a51f0b719532a16a26de41
Arnab Chakraborty <Arnab.Chakraborty@parexelcloud.onmicrosoft.com>
2018-12-31 19:49:34 +0530

    Adding basic directive.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BasicDirective } from './directives/basic.custom.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/directives/basic.custom.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, OnInit, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[basic-directive]'
+})
+
+export class BasicDirective implements OnInit {
+
+  constructor(private elementref: ElementRef) {}
+  ngOnInit() {
+    this.elementref.nativeElement.style.backgroundColor = 'green';
+  }
+}
