8349ea1aba26d2c1c305e61477d0bd15a15aced7
Arnab <grinit10@gmail.com>
2019-03-12 05:29:42 +1100

    Adding async validator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { promise } from 'protractor';
 
 @Component({
   selector: 'app-root',
@@ -9,15 +11,37 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent {
   genders = ['male', 'female'];
   public signupForm: FormGroup;
+  public unallowednames: Array<string> = ['Arnab', 'Arpita'];
 
   constructor() {
     this.signupForm = new FormGroup({
       userdata: new FormGroup({
-        user: new FormControl(null, Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email])
+        user: new FormControl(null, [Validators.required, this.unallowedNamesValidator.bind(this)]),
+        email: new FormControl(null, [Validators.required, Validators.email], this.invalidEmailValidatorAsync)
       }),
       gender: new FormControl('male')
     });
+  }
+
+  invalidEmailValidatorAsync = (control: FormControl): Promise<any> | Observable<any> => {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ 'ForbiddenEmailPresent': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+    return promise;
+  }
+
+  unallowedNamesValidator = (control: FormControl): any => {
+    if (this.unallowednames.find(name => name === control.value)) {
+      return { 'Forbidden name present': true };
+    } else {
+      return null;
+    }
   }
 
   onSubmit = () => console.log(this.signupForm);
